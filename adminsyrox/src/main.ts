9ad4bda20e3128c9fa272f5b99eec78f5734b18b
CarolinaAT <carolinaarizat93@gmail.com>
2026-02-24 13:50:38 -0300

    chore(cors): configure CORS in main for cross-origin requests - change URL

--- adminsyrox/src/main.ts
+++ adminsyrox/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   app.enableCors({
-    origin: 'https://ecommerce-syrox.vercel.app/',
+    origin: 'https://ecommerce-syrox.vercel.app',
     credentials: true,
   });
 
